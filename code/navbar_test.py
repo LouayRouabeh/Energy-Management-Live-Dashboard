f0eb8801b5318ab16bb3a02e9d2b05ae85971703
rouabeh louay <rouabehlouay@medtech.tn>
2021-05-02 18:55:22 +0100

    live updating scatter graph

--- code/navbar_test.py
+++ code/navbar_test.py
@@ -1,16 +1,21 @@
+
+from collections import deque
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
+import plotly
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 import pandas as pd
 from FirstFloor import content1
 from GroundFloor import content0
+import random
 
 first = pd.read_excel("consumption.xlsx", "FirstFloor")
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 
 # styling the sidebar
@@ -75,8 +80,11 @@ def render_page_content(pathname):
         ]
     elif pathname == "/floor-2":
         return [
-            html.H1('consumption and cost of electricity in second floor',
-                    style={'textAlign': 'center'}),
+            dcc.Graph(id='live-graph', animate=True),
+            dcc.Interval(
+                id='graph-update',
+                interval=1000
+            ),
 
         ]
     # If the user tries to reach a different page, return a 404 message
@@ -88,6 +96,27 @@ def render_page_content(pathname):
         ]
     )
 
+X = deque(maxlen=20)
+Y = deque(maxlen=20)
+X.append(1)
+Y.append(1)
+
+
+@app.callback(Output('live-graph', 'figure'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph(input_data):
+
+    X.append(X[-1]+1)
+    Y.append(Y[-1]+(Y[-1]*random.uniform(-0.1, 0.1)))
+    data = plotly.graph_objs.Scatter(
+        x=list(X),
+        y=list(Y),
+        name='Scatter',
+        mode='lines+markers'
+    )
+
+    return {'data': [data], 'layout': go.Layout(xaxis=dict(range=[min(X), max(X)]),
+                                                yaxis=dict(range=[min(Y), max(Y)]), )}
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=3000)
